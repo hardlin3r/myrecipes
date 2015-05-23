a5bf48ec73a7d25c7fd90e8feaa303672f2d60d9
Aldar Antonov <aantonov84@gmail.com>
2015-05-23 17:56:41 +0000

    add image uploader

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -34,6 +34,6 @@ class RecipesController < ApplicationController
   private
   
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
+      params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
 end
